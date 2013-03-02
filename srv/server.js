9813b4bb2cb596888ddef3cd3446d8a945adb9a1
Seamus O'Keefe <okeefe77@gmail.com>
2013-03-02 15:24:28 -0500

    server now accepts and saves font data changes, added underscore

--- srv/server.js
+++ srv/server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose'),
+    _ = require('./lib/underscore-min'),
     port = 7200,
     root = __dirname;
 
@@ -8,12 +9,13 @@ var app = express();
 //CORS Middleware
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 };
 
 app.configure(function() {
+    app.use(express.methodOverride());
     app.use(allowCrossDomain);
 });
 
@@ -31,6 +33,12 @@ var FontSchema = new mongoose.Schema({
 
 var Font = mongoose.model('Font', FontSchema);
 
+
+//Return an OK for OPTIONS requests
+app.options('*', function(req, res) {
+    res.send(200);
+});
+
 //Return full list of fonts
 app.get('/api/fonts', function(req, res) {
     Font.find(function(err, fonts) {
@@ -41,6 +49,25 @@ app.get('/api/fonts', function(req, res) {
         }
     });
 });
+
+//Update a font document
+app.put('/api/fonts/:id', express.bodyParser(), function(req, res) {
+    console.log('Updating ' + req.body.family);
+    Font.findById(req.params.id, function(err, font) {
+        _.extend(font, req.body);
+
+        font.save(function(err) {
+            if(!err) {
+                console.log(font.family + ' update complete');
+            } else {
+                console.log(err);
+            }
+
+            res.send(font);
+        });
+    });
+});
+
 
 //Save a single, new font (likely unneeded)
 app.post('/api/font', express.bodyParser(), function(req, res) {
